29e47fd4092c6331f4b0a925b5dd9ba353f7b259
EASYDEVOR <nathanieldan2002@gmail.com>
2026-04-01 00:20:09 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   turbopack: {},
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-      net: false,
-      tls: false,
-      'react-native': false,
-      '@react-native-async-storage/async-storage': false,
-    };
-    config.externals.push('pino-pretty', 'lokijs', 'encoding');
-    return config;
-  },
 };
 module.exports = nextConfig;
+ 
