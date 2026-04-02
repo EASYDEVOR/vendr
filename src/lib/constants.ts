4272bde2628b9b18d51d5c53f98f20ccaab179f8
EASYDEVOR <nathanieldan2002@gmail.com>
2026-04-02 15:14:02 +0100

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -24,7 +24,7 @@ export const KNOWN_TOKENS: Record<string, { name: string; symbol: string; color:
   '0xa667073d044dd24ea9f1cd932ff9c0c9b983ecd3': { name: 'Tether USD',    symbol: 'USDT', color: '#26A17B', verified: true  },
   '0xc9f9c86933092bbbfff3ccb4b105a4a94bf3bd4e': { name: 'Tesla Token',   symbol: 'TSLA', color: '#CC0000', verified: true  },
   '0x71178bac73cbeb415514eb542a8995b82669778d': { name: 'AMD Token',      symbol: 'AMD',  color: '#ED1C24', verified: true  },
-  '0x5884ad2f920c162cfbbaccc88c9c51aa75ec09e02': { name: 'Amazon Token', symbol: 'AMZN', color: '#FF9900', verified: true  },
+  '0x5884ad2f920c162cfbbacc88c9c51aa75ec09e02': { name: 'Amazon Token', symbol: 'AMZN', color: '#FF9900', verified: true  },
   '0x3b8262a63d25f0477c4dde23f83cfe22cb768c93': { name: 'Netflix Token',  symbol: 'NFLX', color: '#E50914', verified: true  },
   '0x1fbe1a0e43594b3455993b5de5fd0a7a266298d0': { name: 'Palantir Token', symbol: 'PLTR', color: '#7289DA', verified: true  },
 };
